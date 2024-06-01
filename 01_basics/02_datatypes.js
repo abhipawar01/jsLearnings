e083cfd1208b0e653ab077b5c33ff0a9c3860090
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-06-01 03:36:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+//treat all the js code as a newer version code.
+
+//alert(3+3);
+//can not use alert like this because it do not work in node js as browser.
+
+// datatypes in js 
+
+// number => 2 to power 53
+// BIGINT 
+// string => " "
+// Boolean => true/false
+// null => standalone value 
+// undefined =>
+// Symbol => unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
