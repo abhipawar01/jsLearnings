f572aae7a4009ecf875cc591aa5adf67219029c5
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-06-12 12:36:14 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,21 @@
+let score = "abc33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber)
+//console.log(score)
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+//console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 88
+
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof(stringNumber));
