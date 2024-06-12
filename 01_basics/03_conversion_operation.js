0322378d6c9360d8322d0e866a8bc8e454da78d4
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-06-12 13:19:57 +0000

    operations

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -17,5 +17,38 @@ let booleanIsLoggedIn = Boolean(isloggedIn);
 let someNumber = 88
 
 let stringNumber = String(someNumber);
-console.log(someNumber);
-console.log(typeof(stringNumber));
+//console.log(someNumber);
+//console.log(typeof(stringNumber));
+
+
+
+// OPERATIONS--------------------------------------------------
+
+let value = 3
+
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Abhi"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log((3 + (2 * 4)) / 2);
+
+
+
