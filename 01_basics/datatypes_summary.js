fcbcd8c9dfa6378198662b7d6d602b24bfbbc67f
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-06-29 12:52:17 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,11 @@
+// Datatypes in JS
+
+// 1 Primitive datatypes(call By Value )
+// 7 datatypes : Number,String,BigInt,Boolean,Symbol,Undefined,Null
+
+//2 Non-Primitive Datatypes(Call By Refrence)
+//Array,Objects,Functions
+
+const score = 100
+// (as js is a dynamic lang it automatically gets the dataype of the value stored by seeing the value itself we do not need to define it seprately)
+
