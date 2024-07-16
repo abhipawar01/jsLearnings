bbb7e8c785e1d8cee5e23e428c4696481c55a2ed
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-16 16:09:20 +0000

    Strings in JS

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "Abhishek"
+const score = 20
+//console.log(name + score);
+
+//console.log('hello my name is ${name} and my score is ${score}');
+
+const gameName = new String('Abhishek')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+
