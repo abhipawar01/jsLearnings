a2d37ecf8bbe4f3141e0466ef097762f22f4373d
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-18 12:39:51 +0000

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+//IIFE
+
+(function chai()
+{
+    console.log(`DB CONNECTED`)
+})();
+
+(() => {
+    console.log(`DB CONNECTED`)
+})();
+
+( function aurcode(num){
+    console.log(`Number is, ${num}`)
+})(3);
+
