d98ae22a00c9c4343e82f3ae3c1f05a2cf17096f
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-18 05:31:21 +0000

    function in js basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,43 @@
+
+// console.log("A")
+// console.log("B")
+// console.log("H")
+// console.log("I")
+
+function SayMyName()
+{
+    console.log("A")
+    console.log("B")
+    console.log("H")
+    console.log("I")
+}
+//calling of function
+// SayMyName()
+
+// function addTwoNum(num1 ,num2)
+// {
+//     console.log(num1 + num2);
+// }
+
+// addTwoNum(2,3)
+
+//function returning value
+
+function returnsum(num1 ,  num2){
+    return num1 + num2
+}
+
+const result = returnsum(2,3);
+// console.log(result)
+
+// function LoginUserMsg(username= "sam"){
+//     if(!username)
+//     {
+//         console.log("Please enter valid username")
+//         return
+//     }
+//     return `${username} is Logged in`
+// }
+
+// console.log(LoginUserMsg())
+
