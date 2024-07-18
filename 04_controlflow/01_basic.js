413221c778a708dc24fc03d23fba0ebaf847ab49
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-18 15:29:48 +0000

    controlflow js

---
+++ 04_controlflow/01_basic.js
@@ -0,0 +1,20 @@
+//if /if-else/ if-else if / statements and its use with different comparison operators
+
+//switch case statement
+const month =1
+
+switch(month){
+    case 1:
+        console.log("Jan");
+            break;
+    case 2:
+        console.log("Feb");
+            break;
+    case 3:
+        console.log("March");
+            break;
+
+    default:
+        console.log("Enter Valid Number");
+        break;      
+}
