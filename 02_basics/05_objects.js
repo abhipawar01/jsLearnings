bdea6876a32f9bdfcb2d4334717c417f12472cf1
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-18 04:57:04 +0000

    Destructuring of objects and Intro To API

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,12 @@
+const course = {
+    courseName: "Js",
+    price: "999",
+    courseInstructor: "Abhi"
+}
+
+// course.price
+
+const {courseName: CN} = course
+console.log(CN)
+
+//API Application Programming Interface
