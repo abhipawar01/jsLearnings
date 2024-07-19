a78738d969096e98cffad16199184583e402466b
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-19 04:30:30 +0000

    some operators and if else switch

---
+++ 04_controlflow/02_Unique_operators.js
@@ -0,0 +1,14 @@
+// Nullish operator
+let val1 
+// val1 = 5 ?? 10
+val1 = null ?? 10
+// console.log(val1)
+
+//Terniary operator
+
+//condition ? true : false
+
+const iceprice = 100
+
+// iceprice >= 50 ? console.log("Price is higher ") : console.log("Price is Lower")
+
