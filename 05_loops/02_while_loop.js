dc046444a59a2873fa858f158571faca8586222e
Abhishek Pawar <118180360+abhipawar01@users.noreply.github.com>
2024-07-19 05:01:33 +0000

    while do while in js

---
+++ 05_loops/02_while_loop.js
@@ -0,0 +1,17 @@
+//while loop
+
+// let i=0;
+// while(i < 5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+//do-while loop
+let score = 1
+
+do{
+    console.log(score);
+    score++
+}
+while(score < 10)
